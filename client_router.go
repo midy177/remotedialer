3e050588904464913e4390e75ea7a146bc71ec9a
midmly <1228022817@qq.com>
2023-09-04 16:07:31 +0800

    Add Hijack

--- client_router.go
+++ client_router.go
@@ -1,17 +1,20 @@
 package remotedialer
 
 import (
+	"bufio"
 	"fmt"
 	"github.com/labstack/echo/v4"
 	"io"
+	"net"
 	"net/http"
 	"sync/atomic"
+	"time"
 )
 
 var ClientRouter = echo.New()
 
 type connResponseWriter struct {
-	conn   io.Writer
+	conn   io.ReadWriter
 	status int32
 	header http.Header
 }
@@ -40,4 +43,40 @@ func (w *connResponseWriter) WriteHeader(statusCode int) {
 	_, _ = fmt.Fprintf(w.conn, "HTTP/1.1 %d %s\r\n", statusCode, http.StatusText(statusCode))
 	w.Header().Write(w.conn)
 	_, _ = fmt.Fprint(w.conn, "\r\n")
+}
+
+func (w *connResponseWriter) Hijack() (net.Conn, *bufio.ReadWriter, error) {
+
+	return &readWriterConn{w.conn}, bufio.NewReadWriter(bufio.NewReader(w.conn), bufio.NewWriter(w.conn)), nil
+}
+
+type readWriterConn struct {
+	io.ReadWriter
+}
+
+func (rwc *readWriterConn) Close() error {
+	if closer, ok := rwc.ReadWriter.(io.Closer); ok {
+		return closer.Close()
+	}
+	return nil
+}
+
+func (rwc *readWriterConn) LocalAddr() net.Addr {
+	return nil
+}
+
+func (rwc *readWriterConn) RemoteAddr() net.Addr {
+	return nil
+}
+
+func (rwc *readWriterConn) SetDeadline(t time.Time) error {
+	return nil
+}
+
+func (rwc *readWriterConn) SetReadDeadline(t time.Time) error {
+	return nil
+}
+
+func (rwc *readWriterConn) SetWriteDeadline(t time.Time) error {
+	return nil
 }
